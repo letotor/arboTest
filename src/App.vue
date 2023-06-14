686594d5951fac3f05151b6dbd5034f3f22c908b
victorDG <victor.degermain@energieteam.fr>
2023-06-14 14:20:22 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <br/>
+        <RouterView/>
+      </nav>
+    </div>
+  </header>
+</template>
+
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+<style lang="scss">
+@use './assets/base.scss' as *;
+
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Welcome
+        {{ ACCESS_TOKEN }}
+    </div>
+</template>
+
+<script setup lang="ts">
+ const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
